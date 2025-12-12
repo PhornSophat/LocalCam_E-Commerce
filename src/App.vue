920169987ee08efb5ab51c790a27d7e1de68566b
nightnine82 <sophathphorn04gmial.com>
2025-12-13 01:03:31 +0700

    Add footer component, email input, and multiple background images; enhance HomeView with testimonials and work process sections

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,16 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import NavigationBar from './components/NavigationBar.vue';
+import Footer from './components/Footer.vue';
+
 </script>
 
 <template>
   <NavigationBar />
 
   <RouterView />
-  <!-- <div id="app">
-    <router-view />
-  </div> -->
+  
+  <Footer />
 </template>
 
 <style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,19 @@
 <script>
 import { defineComponent } from 'vue'
-// 1. Import the Slider component
+// 1. Import the component
 import Slider from '../components/Slider.vue'
 import NavigationBar from '../components/NavigationBar.vue'
-import Carousel from "../components/Carousel.vue"
+import Carousel from '../components/Carousel.vue'
+import ProductScroll from '../components/ProductScroll.vue'
+import WorkProcess from '../components/WorkProcess.vue'
+import TestimonialCard from '../components/TestimonialCard.vue'
+// 2. Import the Icon
 import { ArrowLongRightIcon } from '@heroicons/vue/24/solid'
+// 3. Import the image
 import categories from '@/assets/categoryGiftSet2.png'
 import Handicraft from '@/assets/categoryGiftSet.png'
 import Toy from '@/assets/homePage.jpg'
 import food from '@/assets/homePage2.jpg'
-import ProductScroll from '@/components/ProductScroll.vue'
 
 export default defineComponent({
   name: 'HomeView',
@@ -20,33 +24,86 @@ export default defineComponent({
     ArrowLongRightIcon,
     Carousel,
     ProductScroll,
+    WorkProcess,
+    TestimonialCard,
   },
 
- setup() {
+  setup() {
+    const testimonialData = [
+      {
+        image: categories,
+        name: 'Chenda',
+        location: 'Phnom Penh, Cambodia',
+        review: 'I bought the roasted cashew and the flavor is amazing!',
+        rating: 5,
+      },
+      {
+        image: Toy,
+        name: 'Chenda',
+        location: 'Phnom Penh, Cambodia',
+        review: 'I bought the roasted cashew and the flavor is amazing!',
+        rating: 5,
+      },
+      {
+        image: food,
+        name: 'Chenda',
+        location: 'Phnom Penh, Cambodia',
+        review: 'I bought the roasted cashew and the flavor is amazing!',
+        rating: 4,
+      },
+    ]
+
+    const Principle = [
+      {
+        number: 1,
+        detail:
+          'Discover a curated collection of designs for every taste and lifestyle. Whether you prefer modern, classic, or eclectic styles,find the perfect pieces to suit your personality and elevate yourspace.',
+        article: 'BROWSE OUR CURATED DESIGNS',
+      },
+      {
+        number: 2,
+        detail:
+          'Two Discover a curated collection of designs for every taste and lifestyle. Whether you prefer modern, classic, or eclectic styles,find the perfect pieces to suit your personality and elevate yourspace.',
+        article: 'Article Two',
+      },
+      {
+        number: 3,
+        detail:
+          'Three Discover a curated collection of designs for every taste and lifestyle. Whether you prefer modern, classic, or eclectic styles,find the perfect pieces to suit your personality and elevate yourspace.',
+        article: 'Article Three',
+      },
+      {
+        number: 4,
+        detail:
+          'Four Discover a curated collection of designs for every taste and lifestyle. Whether you prefer modern, classic, or eclectic styles,find the perfect pieces to suit your personality and elevate yourspace.',
+        article: 'Article Four',
+      },
+    ]
+
     const myCategories = [
-      { name: "Handicraft & Arts", image: categories },
-      { name: "Foods & Snacks", image: food },
-      { name: "Clothing & Accessories", image: Toy },
-      { name: "Beauty & Herbal Products", image: Handicraft },
-      { name: "Home & Decor", image: Handicraft },
-      { name: "Drinks & Coffee", image: Handicraft },
-      { name: "Gift Sets", image: Handicraft },
-    ];
+      { name: 'Handicraft & Arts', image: categories },
+      { name: 'Foods & Snacks', image: food },
+      { name: 'Clothing & Accessories', image: Toy },
+      { name: 'Beauty & Herbal Products', image: Handicraft },
+      { name: 'Home & Decor', image: Handicraft },
+      { name: 'Drinks & Coffee', image: Handicraft },
+      { name: 'Gift Sets', image: Handicraft },
+    ]
 
     const productList = [
-       { img: categories, category: "Handicrafts & Arts", title: "Product 1", price: 12 },
-       { img: food, category: "Foods & Snacks", title: "Product 2", price: 12 },
-       { img: categories, category: "Clothing & Accessories", title: "Product 3", price: 12 },
-       { img: categories, category: "Beauty & Herbal Products", title: "Product 4", price: 12 },
-       { img: categories, category: "Home & Decor", title: "Product 5", price: 12 },
-       { img: categories, category: "Drinks & Decor", title: "Product 6", price: 12 },
-       { img: categories, category: "Gift Sets", title: "Product 7", price: 12 },
-       { img: categories, category: "Handicrafts & Arts", title: "Product 8", price: 12 },
-       { img: categories, category: "Foods & Snacks", title: "Product 9", price: 12 },
-       { img: categories, category: "Home & Decor", title: "Product 10", price: 12 },
-    ];
+      { img: categories, category: 'Handicrafts & Arts', title: 'Product 1', price: 12 },
+      { img: food, category: 'Foods & Snacks', title: 'Product 2', price: 12 },
+      { img: categories, category: 'Clothing & Accessories', title: 'Product 3', price: 12 },
+      { img: categories, category: 'Beauty & Herbal Products', title: 'Product 4', price: 12 },
+      { img: categories, category: 'Home & Decor', title: 'Product 5', price: 12 },
+      { img: categories, category: 'Drinks & Decor', title: 'Product 6', price: 12 },
+      { img: categories, category: 'Gift Sets', title: 'Product 7', price: 12 },
+      { img: categories, category: 'Handicrafts & Arts', title: 'Product 8', price: 12 },
+      { img: categories, category: 'Foods & Snacks', title: 'Product 9', price: 12 },
+      { img: categories, category: 'Home & Decor', title: 'Product 10', price: 12 },
+    ]
 
-    return { myCategories, productList };
+    return { myCategories, productList, Principle, testimonialData }
   },
 })
 </script>
@@ -85,11 +142,12 @@ export default defineComponent({
     <div>/introduction</div>
   </div>
 
-
-<!-- about LocalCam -->
-  <div class="w-full flex max-md:flex-col max-md:justify-center max-md:items-center px-[7vw] py-20 gap-x-[3vw] text-black transition-transform duration-300">
+  <!-- about LocalCam -->
+  <div
+    class="w-full flex max-md:flex-col max-md:justify-center max-md:items-center px-[7vw] py-20 gap-x-[3vw] text-black transition-transform duration-300"
+  >
     <div class="flex flex-col w-1/2 max-md:w-full max-md:items-center">
-      <p class="font-['Condiment'] text-5xl font-bold">about LocalCam</p>
+      <p class="font-['Condiment'] text-[5vw] max-lg:text-5xl font-bold">About LocalCam</p>
       <p class="text-[2vw] max-md:text-xl text-[#767064] italic md:hidden">
         YOUR ONE-STOP DESTINATION FOR ALL THINGS YOU NEEDS.
       </p>
@@ -138,27 +196,191 @@ export default defineComponent({
       <div>///002</div>
       <div>/categories</div>
     </div>
-     <div>
+    <div>
       <Carousel :categories="myCategories" />
     </div>
   </div>
 
-
-<!-- fresh find just in -->
+  <!-- fresh find just in -->
   <div class="w-full">
     <div class="flex justify-between h-auto w-full py-8 text-gray-500 px-[7vw]">
       <div>///003</div>
       <div>/new arrivals</div>
     </div>
-    <div class="text-5xl font-['Condiment'] font-extrabold flex justify-center items-center py-6">
-      fresh find just in
+    <div
+      class="text-[5vw] max-lg:text-5xl font-['Condiment'] font-extrabold flex justify-center items-center py-6"
+    >
+      Fresh Find Just In
     </div>
     <!-- <div class="flex flex-wrap w-full p-10 gap-6 justify-center"> -->
     <div class="w-full px-[7vw]">
-      <ProductScroll :products="productList"/>
+      <ProductScroll :products="productList" />
     </div>
-    <div class="w-full h-screen">
+  </div>
 
+  <!-- feature in -->
+  <div class="flex flex-col w-full my-20">
+    <div
+      class="text-[5vw] max-lg:text-5xl font-['Condiment'] font-extrabold flex justify-center items-center"
+    >
+      Featured In
+    </div>
+    <div class="text-base font-['Poppins'] font-light flex justify-center items-center">
+      Handpicked pieces our customers can’t stop talking about.
+    </div>
+    <div class="w-full px-[7vw] pt-10">
+      <img src="../assets/featureIn.png" alt="Feature In Image" class="obtain-cover" />
+    </div>
+    <div class="flex max-lg:flex-col w-full px-[7vw] py-6">
+      <div class="w-1/2 max-lg:w-full">
+        <p class="font-['Poppins'] text-3xl font-bold">HANDWOVEN CRAFTS</p>
+        <p class="font-['Poppins'] text-3xl py-6">$2-$100</p>
+        <p class="font-['Poppins'] text-base text-gray-500">
+          This vibrant market display offers a wide selection of handcrafted rattan and bamboo
+          goods, from decorative lanterns to practical storage baskets, perfect for adding an
+          authentic, natural aesthetic to any home.
+        </p>
+      </div>
+      <div class="flex flex-col w-1/2 max-lg:w-full px-6 justify-center pt-4">
+        <ul class="list-disc font-semibold">
+          <li>Abundance of Handwoven Goods</li>
+          <li>Cultural Market Setting</li>
+          <li>Colorful Lanterns</li>
+        </ul>
+        <router-link to="/shop">
+          <button
+            class="group w-fit flex justify-center items-center bg-black hover:bg-white text-white font-semibold border border-[#806E53] rounded-lg px-6 py-3 text-lg hover:text-black transition duration-300 cursor-pointer max-[430px]:text-xs max-[430px]:px-4 max-[430px]:py-2 mt-6"
+          >
+            SHOP NOW
+            <ArrowLongRightIcon
+              alt="Go to exploration our products"
+              class="ml-3 w-6 h-6 text-white transition-transform duration-300 group-hover:translate-x-2 group-hover:text-black"
+            />
+          </button>
+        </router-link>
+      </div>
+    </div>
+  </div>
+
+  <!-- work process -->
+  <div class="w-full bg-[#F1EEE5] pb-20">
+    <div class="flex justify-between h-auto w-full pt-14 text-gray-500 px-[7vw]">
+      <div>///005</div>
+      <div>/work process</div>
+    </div>
+    <div class="flex max-lg:flex-col w-full px-[7vw] pt-12">
+      <div
+        class="flex w-1/2 text-[5vw] max-lg:w-full max-lg:text-5xl font-['Condiment'] font-extrabold py-6 max-lg:mb-10 lg:items-center"
+      >
+        a seamless experience made for you
+      </div>
+      <div class="w-1/2 max-lg:w-full">
+        <WorkProcess :priciple="Principle" />
+      </div>
+    </div>
+  </div>
+
+  <!-- testimonials -->
+  <div class="w-full h-auto pb-20 px-[7vw]">
+    <div class="flex justify-between h-auto w-full pt-14 text-gray-500">
+      <div>///006</div>
+      <div>/testimonials</div>
+    </div>
+    <div class="flex flex-col w-full pt-12">
+      <div
+        class="w-full text-[5vw] flex justify-center max-lg:w-full max-lg:text-5xl font-['Condiment'] font-extrabold py-6 pb-8"
+      >
+        What our customers say
+      </div>
+    </div>
+    <div class="w-full flex justify-center">
+      <TestimonialCard :testimonials="testimonialData" />
+    </div>
+  </div>
+
+  <!-- our partner -->
+  <div class="w-full bg-[#F1EEE5] px-[7vw] pb-20">
+    <div class="flex justify-between h-auto w-full pt-14 text-gray-500 px-[7vw]">
+      <div>///007</div>
+      <div>/our partners</div>
+    </div>
+    <div
+      class="w-full text-[5vw] flex justify-center max-lg:w-full max-lg:text-5xl font-['Condiment'] font-extrabold py-6 pb-8"
+    >
+      Trust by Hundreds
+    </div>
+    <div class="w-full flex">
+      <div
+        class="w-1/4 flex justify-center items-center gap-4 border-r-2 border-[#D5D5D2] max-lg:flex-col"
+      >
+        <img src="../assets/partner1 1.png" alt="confirel" class="w-[6vw]" />
+        <div class="text-3xl max-lg:text-xl text-[#616161] font-['Poppins'] font-bold">
+          confirel
+        </div>
+      </div>
+      <div
+        class="w-1/4 flex justify-center items-center gap-4 border-r-2 border-[#D5D5D2] max-lg:flex-col"
+      >
+        <img src="../assets/partner1 1.png" alt="confirel" class="w-[6vw]" />
+        <div class="text-3xl max-lg:text-xl text-[#616161] font-['Poppins'] font-bold">
+          confirel
+        </div>
+      </div>
+      <div
+        class="w-1/4 flex justify-center items-center gap-4 border-r-2 border-[#D5D5D2] max-lg:flex-col"
+      >
+        <img src="../assets/partner1 1.png" alt="confirel" class="w-[6vw]" />
+        <div class="text-3xl max-lg:text-xl text-[#616161] font-['Poppins'] font-bold">
+          confirel
+        </div>
+      </div>
+      <div class="w-1/4 flex justify-center items-center gap-4 max-lg:flex-col">
+        <img src="../assets/partner1 1.png" alt="confirel" class="w-[6vw]" />
+        <div class="text-3xl max-lg:text-xl text-[#616161] font-['Poppins'] font-bold">
+          confirel
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Background Image -->
+  <img
+    src="../assets/bgImage4.jpg"
+    alt="Background Image"
+    class="w-full h-[35vw] object-cover sticky top-0 z-0"
+  />
+
+
+  <div class="w-full bg-[#F1EEE5] pb-20 relative z-10">
+    <div class="flex justify-between h-auto w-full pt-14 text-gray-500 px-[7vw]">
+      <div>///007</div>
+      <div>/shopping</div>
+    </div>
+    <div class="flex max-lg:flex-col-reverse w-full px-[7vw] pt-12">
+      <div
+        class="flex flex-col w-1/2 text-[5vw] max-lg:w-full max-lg:text-5xl font-['Condiment'] font-extrabold py-6 max-lg:mb-10 lg:items-s max-lg:text-center max-lg:leading-normal"
+      >
+        Ready to refresh your lifestyle with local goodness?
+        <router-link to="/shop">
+          <button
+            class="font-['Poppins'] group w-fit flex justify-start items-start bg-black hover:bg-white text-white font-semibold border border-[#806E53] rounded-lg px-6 py-3 text-lg hover:text-black transition duration-300 cursor-pointer max-[430px]:text-xs max-[430px]:px-4 max-[430px]:py-2 mt-6"
+          >
+            START SHOPPING NOW
+            <ArrowLongRightIcon
+              alt="Go to exploration our products"
+              class="ml-3 w-6 h-6 text-white transition-transform duration-300 group-hover:translate-x-2 group-hover:text-black"
+            />
+          </button>
+        </router-link>
+      </div>
+      <div class="flex w-1/2 max-lg:w-full p-[4vw] gap-6">
+        <div class="w-1/2">
+          <img src="../assets/image 20.png" alt="" class="w-full" />
+        </div>
+        <div class="w-1/2">
+          <img src="../assets/image 21.png" alt="" class="w-full" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
